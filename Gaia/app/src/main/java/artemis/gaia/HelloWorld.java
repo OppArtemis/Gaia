de959f6f47796f00b65d53dcb3d5ef2152556cb4
Jonathan Lin <jonathan.fs.lin@gmail.com>
2017-01-10 21:43:40 -0500

    Button
    Implemented a button that polls a text field and responds with a toast
    popup

--- Gaia/app/src/main/java/artemis/gaia/HelloWorld.java
+++ Gaia/app/src/main/java/artemis/gaia/HelloWorld.java
@@ -4,9 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.net.Uri;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.database.Cursor;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class HelloWorld extends AppCompatActivity {
     String msg = "Android : ";
+    EditText searchString;
 
     /** Called when the activity is first created. */
     @Override
@@ -14,40 +23,15 @@ public class HelloWorld extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
         Log.d(msg, "The onCreate() event");
+
+        searchString = (EditText) findViewById(R.id.txt_searchString);
     }
 
-    /** Called when the activity is about to become visible. */
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(msg, "The onStart() event");
-    }
+    public void pollURL(View view) {
+        //  String text = findViewById(R.id.txt_searchString).toString();
+        String text = searchString.getText().toString();
+        Toast.makeText(HelloWorld.this, text, Toast.LENGTH_SHORT).show();
 
-    /** Called when the activity has become visible. */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(msg, "The onResume() event");
-    }
-
-    /** Called when another activity is taking focus. */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(msg, "The onPause() event");
-    }
-
-    /** Called when the activity is no longer visible. */
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(msg, "The onStop() event");
-    }
-
-    /** Called just before the activity is destroyed. */
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(msg, "The onDestroy() event");
+//        findViewById(R.id.txt_verify).set;
     }
 }

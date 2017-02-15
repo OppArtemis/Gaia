09ff920352fb5edd11e80c9bfe7c729abae7eb1d
Jonathan Lin <jonathan.fs.lin@gmail.com>
2017-02-14 21:41:50 -0500

    HTTPgetter
    Added new module, httpgetter, for java-based testing

---
+++ Gaia/artemis/src/main/java/gaia/backend/HTTPGetter.java
@@ -0,0 +1,8 @@
+package gaia.backend;
+
+public class HTTPGetter {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
---
+++ Gaia/httpgetter2/src/main/java/artemis/gaia/HTTPGetter.java
@@ -0,0 +1,4 @@
+package artemis.gaia;
+
+public class HTTPGetter {
+}
